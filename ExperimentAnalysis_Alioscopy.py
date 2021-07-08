809385410b7e87e82fc8277d43e5ab9df1219084
QuentinHuan <quentinhuan@laposte.net>
2021-07-08 10:45:59 +0200

    update alisocopy log path

--- ExperimentAnalysis_Alioscopy.py
+++ ExperimentAnalysis_Alioscopy.py
@@ -16,6 +16,6 @@ def get_script_path():
 wdir=get_script_path()
 os.chdir(wdir)
 print("WORKING DIR ="+wdir)
-main.process_one_log(wdir+"../logs","P3d.log")
+main.process_one_log(wdir+"../logs","p3d.log")
 #main.process_one_log(wdir+"/../../Saved/Logs","P3d_Expe1.log") # path in UE4 project
 print("SAVED")
